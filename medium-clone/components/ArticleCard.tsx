bc4e5a184a76687035fd1700fbd9b4342de5407e
dinie zikry <diniezikry@dinies-MacBook-Pro.local>
2023-01-02 02:46:18 +0800

    completed project

---
+++ medium-clone/components/ArticleCard.tsx
@@ -0,0 +1,36 @@
+import type { NextPage } from "next";
+import { useUser } from "@supabase/auth-helpers-react";
+import { useRouter } from "next/router";
+import { Card, Text } from "@nextui-org/react";
+
+interface Props {
+  article: any;
+}
+
+const ArticleCard: NextPage<Props> = (props) => {
+  const router = useRouter();
+  const { article } = props;
+
+  function getDate() {
+    let time = Date.parse(article.inserted_at);
+    let date = new Date(time);
+
+    return date.getDay() + "-" + date.getMonth() + "-" + date.getFullYear();
+  }
+
+  return (
+    <Card
+      isPressable
+      css={{ mb: "$10" }}
+      onPress={() => router.push("/article?id=" + article.id)}
+    >
+      <Card.Body>
+        <Text h2>{article.title}</Text>
+        <Text b>posted {getDate()}</Text>
+        <Text b>by {article.user_email.toLowerCase()}</Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default ArticleCard;
---
+++ medium-clone/pages/mainFeed/index.tsx
@@ -0,0 +1,47 @@
+import type { NextPage } from "next";
+import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useRouter } from "next/router";
+import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
+import { useState, useEffect } from "react";
+import { Text } from "@nextui-org/react";
+import ArticleCard from "../../components/ArticleCard";
+
+const MainFeed: NextPage = () => {
+  const supabaseClient = useSupabaseClient();
+  const user = useUser();
+  const router = useRouter();
+  const [articles, setArticles] = useState<string[]>([]);
+
+  useEffect(() => {
+    getArticles();
+  }, []);
+
+  const getArticles = async () => {
+    try {
+      const { data, error } = await supabaseClient
+        .from("articles")
+        .select("*")
+        .limit(10);
+
+      if (data != null) {
+        setArticles(data);
+      }
+    } catch (error: any) {
+      alert(error.message);
+    }
+  };
+
+  return (
+    <>
+      <Text h2>Main Feed</Text>
+      <Text size="$lg" css={{ my: "$8" }}>
+        Check out articles from users here
+        {articles.map((article) => (
+          <ArticleCard article={article} />
+        ))}
+      </Text>
+    </>
+  );
+};
+
+export default MainFeed;
